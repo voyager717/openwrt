voyager717/openwrt

commit bc290ed256bc8232243cfd06e8938f3431c6ae95

kernel: add SND_PCM_TIMER to kmod-sound-core
FS#256

Fix audio not working due to unset SND_PCM_TIMER. CONFIG_SND_PCM_TIMER
is required for ALSA to work (at least for some audio devices),
otherwise applications using sound may fail. Can be reproduced by
installing "alsa-utils-tests" and running "speaker-test":

...
ALSA lib pcm_direct.c:1159:(snd1_pcm_direct_initialize_poll_fd) unable to open timer 'hw:CLASS=3,SCLASS=0,CARD=0,DEV=0,SUBDEV=0'
ALSA lib pcm_dmix.c:1106:(snd_pcm_dmix_open) unable to initialize poll_fd
Playback open error: -19,No such device

The kernel size does not increase, only the size of the kernel module
increases.

Signed-off-by: Dmitry Sutyagin <f3flight@gmail.com>

package/kernel/linux/modules/sound.mk
@@ -49,6 +49,7 @@ define KernelPackage/sound-core
 	CONFIG_SND_RAWMIDI \
 	CONFIG_SND_TIMER \
 	CONFIG_SND_PCM \
+	CONFIG_SND_PCM_TIMER \
 	CONFIG_SND_SEQUENCER \
 	CONFIG_SND_VIRMIDI \
 	CONFIG_SND_SEQ_DUMMY \

commit 13aa518996db825d7d762519d1f38451db625bd3

kernel: fix kmod-sound-core build error
Fix build error introduced with bc290ed.

Signed-off-by: Mathias Kresin <dev@kresin.me>

package/kernel/linux/modules/sound.mk
@@ -49,7 +49,7 @@ define KernelPackage/sound-core
 	CONFIG_SND_RAWMIDI \
 	CONFIG_SND_TIMER \
 	CONFIG_SND_PCM \
-	CONFIG_SND_PCM_TIMER \
+	CONFIG_SND_PCM_TIMER=y \
 	CONFIG_SND_SEQUENCER \
 	CONFIG_SND_VIRMIDI \
 	CONFIG_SND_SEQ_DUMMY \