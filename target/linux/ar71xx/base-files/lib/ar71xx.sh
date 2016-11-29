voyager717/openwrt

commit e42e5952464dab13f544bfd8760a53f61065ffe2

ar71xx: add model detection for UBNT Rocket Ti
Based-on-patch-by: Neal Oakey <neal.oakey@bingo-ev.de>
Signed-off-by: Matthias Schiffer <mschiffer@universe-factory.net>

target/linux/ar71xx/base-files/lib/ar71xx.sh
@@ -82,7 +82,8 @@ ubnt_xm_board_detect() {
 		"e0a")
 			model="Ubiquiti NanoStation loco M"
 			;;
-		"e1b")  # Rocket M5 untested
+		"e1b"|\ # Note: the M5 / M5 Ti haven't yet been verified
+		"e1d")  # Rocket M Ti
 			model="Ubiquiti Rocket M"
 			;;
 		"e20"|\

commit 24c67adc31a19de94e1f8773d9c6be6809870aef

ar71xx: fix syntax error in /lib/ar71xx.sh
Remove all comments in ubnt_xm_board_detect(), as it's not possible to add
comments to specific lines of the case construct anyways.

Fixes: e42e5952 "ar71xx: add model detection for UBNT Rocket Ti"
Signed-off-by: Matthias Schiffer <mschiffer@universe-factory.net>

target/linux/ar71xx/base-files/lib/ar71xx.sh
@@ -76,18 +76,18 @@ ubnt_xm_board_detect() {
 	case ${magic:0:3} in
 		"e00"|\
 		"e01"|\
-		"e80")  # Different revisions of the NanoStation?
+		"e80")
 			model="Ubiquiti NanoStation M"
 			;;
 		"e0a")
 			model="Ubiquiti NanoStation loco M"
 			;;
-		"e1b"|\ # Note: the M5 / M5 Ti haven't yet been verified
-		"e1d")  # Rocket M Ti
+		"e1b"|\
+		"e1d")
 			model="Ubiquiti Rocket M"
 			;;
 		"e20"|\
-		"e2d")  # Bullet M Ti
+		"e2d")
 			model="Ubiquiti Bullet M"
 			;;
 		"e30")