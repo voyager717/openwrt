voyager717/openwrt

commit 97c78c511e54694148f4166bdedcd54817285fa1

treewide: access device tree from userspace via /proc/
Access the device tree via /proc/device-tree/ is the documented way to
access the properties. Everything else might not work in future.

Signed-off-by: Mathias Kresin <dev@kresin.me>

target/linux/ipq806x/base-files/lib/ipq806x.sh
@@ -72,7 +72,7 @@ ipq806x_board_name() {
 ipq806x_get_dt_led() {
 	local label
 	local ledpath
-	local basepath="/sys/firmware/devicetree/base"
+	local basepath="/proc/device-tree/base"
 	local nodepath="$basepath/aliases/led-$1"
 
 	[ -f "$nodepath" ] && ledpath=$(cat "$nodepath")

target/linux/lantiq/base-files/lib/functions/lantiq.sh
@@ -3,7 +3,7 @@
 lantiq_get_dt_led() {
 	local label
 	local ledpath
-	local basepath="/sys/firmware/devicetree/base"
+	local basepath="/proc/device-tree/base"
 	local nodepath="$basepath/aliases/led-$1"
 
 	[ -f "$nodepath" ] && ledpath=$(cat "$nodepath")

commit aae3318b87a109531cc1e19327ccb41558ec3283

treewide: fix device tree path in scripts
The device tree is at /proc/device-tree/ without a base subdir.

Fixes: 97c78c511e54 ("treewide: access device tree from userspace via /proc/")

Signed-off-by: Mathias Kresin <dev@kresin.me>

target/linux/ipq806x/base-files/lib/ipq806x.sh
@@ -72,7 +72,7 @@ ipq806x_board_name() {
 ipq806x_get_dt_led() {
 	local label
 	local ledpath
-	local basepath="/proc/device-tree/base"
+	local basepath="/proc/device-tree"
 	local nodepath="$basepath/aliases/led-$1"
 
 	[ -f "$nodepath" ] && ledpath=$(cat "$nodepath")

target/linux/lantiq/base-files/lib/functions/lantiq.sh
@@ -3,7 +3,7 @@
 lantiq_get_dt_led() {
 	local label
 	local ledpath
-	local basepath="/proc/device-tree/base"
+	local basepath="/proc/device-tree"
 	local nodepath="$basepath/aliases/led-$1"
 
 	[ -f "$nodepath" ] && ledpath=$(cat "$nodepath")