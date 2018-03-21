voyager717/openwrt

commit 173a30575b52caffb608716a6bf47f58d5d71d00

kernel: b53: look for NVRAM's "robo_reset" entry on every platform
Since kernel 4.1 bcm47xx_nvram_gpio_pin() is now defined in a global
header and can be safely called even on non-Broadcom platforms.

This change makes b53 look for "robo_reset" on ARCH_BCM_5301X and
slightly simplifies the code.

Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

target/linux/generic/files/drivers/net/phy/b53/b53_priv.h
@@ -311,6 +311,7 @@ static inline int b53_write64(struct b53_device *dev, u8 page, u8 reg,
 
 #ifdef CONFIG_BCM47XX
 #include <bcm47xx_board.h>
+#endif
 
 #include <linux/version.h>
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 1, 0))
@@ -318,20 +319,21 @@ static inline int b53_write64(struct b53_device *dev, u8 page, u8 reg,
 #endif
 static inline int b53_switch_get_reset_gpio(struct b53_device *dev)
 {
+#ifdef CONFIG_BCM47XX
 	enum bcm47xx_board board = bcm47xx_board_get();
 
 	switch (board) {
 	case BCM47XX_BOARD_LINKSYS_WRT300NV11:
 	case BCM47XX_BOARD_LINKSYS_WRT310NV1:
 		return 8;
-	default:
-		return bcm47xx_nvram_gpio_pin("robo_reset");
 	}
-}
-#else
-static inline int b53_switch_get_reset_gpio(struct b53_device *dev)
-{
-	return -ENOENT;
-}
 #endif
+
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 1, 0))
+	return bcm47xx_nvram_gpio_pin("robo_reset");
+#else
+	return -ENOENT;
+#endif
+}
+
 #endif

commit fef7720863e31a5eba80ecdc38d7861d3036fc08

kernel: b53: fix compilation on BCM47XX
This fixes:
drivers/net/phy/b53/b53_priv.h:325:2: error: enumeration value '<board>' not handled in switch [-Werror=switch]
errors.

Fixes: 173a30575b52c ("kernel: b53: look for NVRAM's "robo_reset" entry on every platform")
Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

target/linux/generic/files/drivers/net/phy/b53/b53_priv.h
@@ -326,6 +326,8 @@ static inline int b53_switch_get_reset_gpio(struct b53_device *dev)
 	case BCM47XX_BOARD_LINKSYS_WRT300NV11:
 	case BCM47XX_BOARD_LINKSYS_WRT310NV1:
 		return 8;
+	default:
+		break;
 	}
 #endif
 