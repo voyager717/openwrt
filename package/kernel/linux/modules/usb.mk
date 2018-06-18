voyager717/openwrt

commit 9589b0493760b5777a5015a3520f76f5cd15d7d5

kernel/modules: add chaoskey module, hardware TRNG
Package the driver for Chaoskey, a USB dongle that provides a
True Random Number Generator (TRNG) and feeds entropy to kernel.

Chaoskey driver is included the upstream Linux sources, so
only packaging it is needed.

Run-tested with ipq806x/R7800 and mvebu/WRT3200ACM.

(Requires CONFIG_HW_RANDOM kernel option.)

Signed-off-by: Hannu Nyman <hannu.nyman@iki.fi>

package/kernel/linux/modules/usb.mk
@@ -1639,3 +1639,18 @@ endef
 
 $(eval $(call KernelPackage,usb-net2280))
 
+define KernelPackage/chaoskey
+  SUBMENU:=$(USB_MENU)
+  TITLE:=Chaoskey hardware RNG support
+  KCONFIG:=CONFIG_USB_CHAOSKEY
+  FILES:=$(LINUX_DIR)/drivers/usb/misc/chaoskey.ko
+  AUTOLOAD:=$(call AutoProbe,chaoskey)
+  $(call AddDepends/usb)
+endef
+
+define KernelPackage/chaoskey/description
+  Kernel module for chaoskey, USB attached true random number generator
+endef
+
+$(eval $(call KernelPackage,chaoskey))
+

commit 1d058836df145a1dcea6d0868d0cca5cbd20b82e

kernel/modules: kmod-chaoskey: Add missing dependency
This new package was missing the dependency to kmod-random-core which
caused some build errors.

Fixes: 9589b049376 ("kernel/modules: add chaoskey module, hardware TRNG")
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

package/kernel/linux/modules/usb.mk
@@ -1642,6 +1642,7 @@ $(eval $(call KernelPackage,usb-net2280))
 define KernelPackage/chaoskey
   SUBMENU:=$(USB_MENU)
   TITLE:=Chaoskey hardware RNG support
+  DEPENDS:=+kmod-random-core
   KCONFIG:=CONFIG_USB_CHAOSKEY
   FILES:=$(LINUX_DIR)/drivers/usb/misc/chaoskey.ko
   AUTOLOAD:=$(call AutoProbe,chaoskey)