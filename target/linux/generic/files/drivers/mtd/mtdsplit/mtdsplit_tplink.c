voyager717/openwrt

commit f09c9d1d20f4148a7f3f17162f8926ff64d71b3f

kernel: add DT binding support to the TP-LINK parser
It allows triggering it directly by specifying format in the DT.

Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

target/linux/generic/files/drivers/mtd/mtdsplit/mtdsplit_tplink.c
@@ -152,9 +152,19 @@ static int mtdsplit_parse_tplink(struct mtd_info *master,
 	return TPLINK_NR_PARTS;
 }
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0)
+static const struct of_device_id mtdsplit_tplink_of_match_table[] = {
+	{ .compatible = "tplink,firmware" },
+	{},
+};
+#endif
+
 static struct mtd_part_parser mtdsplit_tplink_parser = {
 	.owner = THIS_MODULE,
 	.name = "tplink-fw",
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 0)
+	.of_match_table = mtdsplit_tplink_of_match_table,
+#endif
 	.parse_fn = mtdsplit_parse_tplink,
 	.type = MTD_PARSER_TYPE_FIRMWARE,
 };

commit ab8985cf4a360d8339ef7d30c1dc55a8dfd3e04c

kernel: add missing version.h include to the TP-LINK parser
Fixes: f09c9d1d20f4 ("kernel: add DT binding support to the TP-LINK parser")
Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

target/linux/generic/files/drivers/mtd/mtdsplit/mtdsplit_tplink.c
@@ -14,6 +14,7 @@
 #include <linux/slab.h>
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/partitions.h>
+#include <linux/version.h>
 #include <linux/byteorder/generic.h>
 
 #include "mtdsplit.h"