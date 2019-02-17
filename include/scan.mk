voyager717/openwrt

commit b6d424b44276137a526d496ebab8df35368fbc13

build: Honour NO_COLOR in include/scan.mk
The sender domain has a DMARC Reject/Quarantine policy which disallows
sending mailing list messages using the original "From" header.

To mitigate this problem, the original message has been wrapped
automatically by the mailing list software.
Hi all:

This is my first OpenWrt patch. I am a clean, pure newbie! 8-)

Honour NO_COLOR in Makefile function 'progress' in include/scan.mk, in the same way that include/verbose.mk does.

Signed-off-by: R. Diez <rdiezmail-openwrt@yahoo.com>

include/scan.mk
@@ -19,9 +19,15 @@ else
 endif
 
 ifeq ($(IS_TTY),1)
-  define progress
+  ifneq ($(strip $(NO_COLOR)),1)
+    define progress
 	printf "\033[M\r$(1)" >&2;
-  endef
+    endef
+  else
+    define progress
+	:;
+    endef
+  endif
 else
   define progress
 	:;

commit f9b8e82deffbc511d194a3c3fca1dbbc48324932

build: Fix print without color
The original patch removed the printing completely, just remove the
color.

Fixes: b6d424b44276  ("build: Honour NO_COLOR in include/scan.mk")
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

include/scan.mk
@@ -25,7 +25,7 @@ ifeq ($(IS_TTY),1)
     endef
   else
     define progress
-	:;
+	printf "\r$(1)" >&2;
     endef
   endif
 else