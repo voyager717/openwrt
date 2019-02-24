voyager717/openwrt

commit 265ddadadf301fef48816afa590f25bfcd5dd1ac

kernel: add kmod-input-touchscreen-ads7846
This module adds support for ADS7846 based touchscreens used in devices
like the WaveShare 3.5" and 4" LCD displays designed for Raspberry Pi.

Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

package/kernel/linux/modules/input.mk
@@ -173,6 +173,25 @@ endef
 $(eval $(call KernelPackage,input-matrixkmap))
 
 
+define KernelPackage/input-touchscreen-ads7846
+  SUBMENU:=$(INPUT_MODULES_MENU)
+  TITLE:=ADS7846/TSC2046/AD7873 and AD(S)7843 based touchscreens
+  DEPENDS:=+kmod-hwmon-core +kmod-spi-bitbang
+  KCONFIG:= \
+	CONFIG_INPUT_TOUCHSCREEN=y \
+	CONFIG_TOUCHSCREEN_PROPERTIES=y \
+	CONFIG_TOUCHSCREEN_ADS7846
+  FILES:=$(LINUX_DIR)/drivers/input/touchscreen/ads7846.ko
+  AUTOLOAD:=$(call AutoProbe,ads7846)
+endef
+
+define KernelPackage/input-touchscreen-ads7846/description
+  Kernel module for ADS7846/TSC2046/AD7873 and AD(S)7843 based touchscreens
+endef
+
+$(eval $(call KernelPackage,input-touchscreen-ads7846))
+
+
 define KernelPackage/keyboard-imx
   SUBMENU:=$(INPUT_MODULES_MENU)
   TITLE:=IMX keypad support

target/linux/brcm2708/bcm2708/config-4.14
@@ -385,6 +385,8 @@ CONFIG_TIMER_OF=y
 CONFIG_TIMER_PROBE=y
 CONFIG_TINY_SRCU=y
 CONFIG_TMPFS_POSIX_ACL=y
+# CONFIG_TOUCHSCREEN_EXC3000 is not set
+# CONFIG_TOUCHSCREEN_RPI_FT5406 is not set
 CONFIG_UEVENT_HELPER_PATH=""
 # CONFIG_UID16 is not set
 CONFIG_UNCOMPRESS_INCLUDE="debug/uncompress.h"

target/linux/brcm2708/bcm2709/config-4.14
@@ -425,6 +425,8 @@ CONFIG_TICK_CPU_ACCOUNTING=y
 CONFIG_TIMER_OF=y
 CONFIG_TIMER_PROBE=y
 CONFIG_TMPFS_POSIX_ACL=y
+# CONFIG_TOUCHSCREEN_EXC3000 is not set
+# CONFIG_TOUCHSCREEN_RPI_FT5406 is not set
 CONFIG_TREE_RCU=y
 CONFIG_TREE_SRCU=y
 CONFIG_UEVENT_HELPER_PATH=""

target/linux/brcm2708/bcm2710/config-4.14
@@ -450,6 +450,8 @@ CONFIG_TICK_CPU_ACCOUNTING=y
 CONFIG_TIMER_OF=y
 CONFIG_TIMER_PROBE=y
 CONFIG_TMPFS_POSIX_ACL=y
+# CONFIG_TOUCHSCREEN_EXC3000 is not set
+# CONFIG_TOUCHSCREEN_RPI_FT5406 is not set
 CONFIG_TREE_RCU=y
 CONFIG_TREE_SRCU=y
 CONFIG_UEVENT_HELPER_PATH=""

commit ee833f57c941ad20f9f13c254d830b785d001bed

kernel: fix kmod-input-touchscreen-ads7846 deps
On targets that don't have input support enabled in the kernel config,
building kmod-input-touchscreen-ads7846 fails due to a missing
dependency on kmod-input-core. Add the dependency to fix this.

Fixes: 265ddadadf30 ("kernel: add kmod-input-touchscreen-ads7846")
Reported-by: Hannu Nyman <hannu.nyman@iki.fi>
Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

package/kernel/linux/modules/input.mk
@@ -176,7 +176,7 @@ $(eval $(call KernelPackage,input-matrixkmap))
 define KernelPackage/input-touchscreen-ads7846
   SUBMENU:=$(INPUT_MODULES_MENU)
   TITLE:=ADS7846/TSC2046/AD7873 and AD(S)7843 based touchscreens
-  DEPENDS:=+kmod-hwmon-core +kmod-spi-bitbang
+  DEPENDS:=+kmod-hwmon-core +kmod-input-core +kmod-spi-bitbang
   KCONFIG:= \
 	CONFIG_INPUT_TOUCHSCREEN=y \
 	CONFIG_TOUCHSCREEN_PROPERTIES=y \