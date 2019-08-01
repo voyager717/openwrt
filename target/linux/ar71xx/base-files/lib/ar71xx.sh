voyager717/openwrt

commit 7d71d6638cc2f72e0299355cf4117c49b3e39d25

ar71xx: fix Mikrotik board detection
Fix a typo in the machine type being extracted from /proc/cpuinfo
which causes all Mikrotik board to be undetected properly.

This lead to sysupgrade issues and probably some others too.

Fixes: 7d81bbbe5ece ("ar71xx: base-files: fix board detect on new MikroTik devices")

Signed-off-by: Koen Vandeputte <koen.vandeputte@ncentric.com>

target/linux/ar71xx/base-files/lib/ar71xx.sh
@@ -1001,7 +1001,7 @@ ar71xx_board_detect() {
 	*"MAC1200R")
 		name="mc-mac1200r"
 		;;
-	"MikroTik"*)
+	"Mikrotik"*)
 		name=$(mikrotik_board_detect "$machine")
 		;;
 	*"MiniBox V1.0")

commit a441b2052aa6d4cef44eeb839d171d393910b280

ar71xx: really fix Mikrotik board detection
commit 7d71d6638cc2 ("ar71xx: fix Mikrotik board detection")
was generated based on testing a rb-912 board, on which detection failed.

Testing on more hardware shows something fun:

machine	: MikroTik RouterBOARD 922UAGS-5HPacD
machine	: Mikrotik RouterBOARD 912UAG-5HPnD

Both lowercase and uppercase are used.
So ensure we support both now ..

Fixes: 7d71d6638cc2 ("ar71xx: fix Mikrotik board detection")

Signed-off-by: Koen Vandeputte <koen.vandeputte@ncentric.com>

target/linux/ar71xx/base-files/lib/ar71xx.sh
@@ -1001,6 +1001,7 @@ ar71xx_board_detect() {
 	*"MAC1200R")
 		name="mc-mac1200r"
 		;;
+	"MikroTik"*|\
 	"Mikrotik"*)
 		name=$(mikrotik_board_detect "$machine")
 		;;