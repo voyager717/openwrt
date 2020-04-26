voyager717/openwrt

commit 50531df3138ef00c1e66cc4e5dfb4c904da59e38

kernel: add module to support SFP cages
Enables kernel SFP case support.

Signed-off-by: René van Dorst <opensource@vdorst.com>

package/kernel/linux/modules/netdevices.mk
@@ -1141,3 +1141,23 @@ define KernelPackage/mlx5-core/description
 endef
 
 $(eval $(call KernelPackage,mlx5-core))
+
+
+define KernelPackage/sfp
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  TITLE:=SFP cage support
+  DEPENDS:=+kmod-i2c-core +kmod-hwmon-core
+  KCONFIG:= \
+	CONFIG_SFP=m \
+	CONFIG_MDIO_I2C=m
+  FILES:= \
+	$(LINUX_DIR)/drivers/net/phy/sfp.ko \
+	$(LINUX_DIR)/drivers/net/phy/mdio-i2c.ko
+  AUTOLOAD:=$(call AutoProbe,mdio-i2c sfp)
+endef
+
+define KernelPackage/SFP/description
+ Kernel module to support SFP cages
+endef
+
+$(eval $(call KernelPackage,sfp))

target/linux/ramips/image/mt7621.mk
@@ -820,7 +820,7 @@ TARGET_DEVICES += ubnt_edgerouter-x
 define Device/ubnt_edgerouter-x-sfp
   $(Device/ubnt_edgerouter_common)
   DEVICE_MODEL := EdgeRouter X SFP
-  DEVICE_PACKAGES += kmod-i2c-algo-pca kmod-gpio-pca953x
+  DEVICE_PACKAGES += kmod-i2c-algo-pca kmod-gpio-pca953x kmod-sfp
   SUPPORTED_DEVICES += ubnt-erx-sfp ubiquiti,edgerouterx-sfp
 endef
 TARGET_DEVICES += ubnt_edgerouter-x-sfp

commit 9d0104a2076fc3f6369f608cc89dc347a813e3c1

kernel: netdev: add phylink dependency for sfp
Fixes: 50531df3138e ("kernel: add module to support SFP cages")

Signed-off-by: David Bauer <mail@david-bauer.net>

package/kernel/linux/modules/netdevices.mk
@@ -1162,7 +1162,7 @@ $(eval $(call KernelPackage,mlx5-core))
 define KernelPackage/sfp
   SUBMENU:=$(NETWORK_DEVICES_MENU)
   TITLE:=SFP cage support
-  DEPENDS:=+kmod-i2c-core +kmod-hwmon-core
+  DEPENDS:=+kmod-i2c-core +kmod-hwmon-core +kmod-phylink
   KCONFIG:= \
 	CONFIG_SFP=m \
 	CONFIG_MDIO_I2C=m