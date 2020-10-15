voyager717/openwrt

commit 08feb1f010638c663f199986c49199686053a987

sunxi: BPi-M2U kernel modules for onboard WiFi
Signed-off-by: Hal Martin <hal.martin@gmail.com>

target/linux/sunxi/image/cortexa7.mk
@@ -74,7 +74,8 @@ TARGET_DEVICES += lemaker_bananapi
 define Device/lemaker_bananapi-m2-ultra
   DEVICE_VENDOR := LeMaker
   DEVICE_MODEL := Banana Pi M2 Ultra
-  DEVICE_PACKAGES:=kmod-rtc-sunxi kmod-ata-sunxi
+  DEVICE_PACKAGES:=kmod-rtc-sunxi kmod-ata-sunxi kmod-brcmfmac \
+	brcmfmac-firmware-43430a0-sdio wpad-basic
   SOC := sun8i-r40
 endef
 TARGET_DEVICES += lemaker_bananapi-m2-ultra

commit b29b10b168d344c908588f3f6a5b7bda922088a8

sunxi: use wpad-basic-wolfssl for Sinovoip Banana Pi M2 Ultra
The idea of commit a465ea4442fd was to use wpad-basic-wolfssl
consistently throughout the whole trunk, so use it here as well.

Fixes: 08feb1f01063 ("BPi-M2U kernel modules for onboard WiFi")

Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

target/linux/sunxi/image/cortexa7.mk
@@ -75,7 +75,7 @@ define Device/sinovoip_bananapi-m2-ultra
   DEVICE_VENDOR := Sinovoip
   DEVICE_MODEL := Banana Pi M2 Ultra
   DEVICE_PACKAGES:=kmod-rtc-sunxi kmod-ata-sunxi kmod-brcmfmac \
-	brcmfmac-firmware-43430a0-sdio wpad-basic
+	brcmfmac-firmware-43430a0-sdio wpad-basic-wolfssl
   SUPPORTED_DEVICES:=lemaker,bananapi-m2-ultra
   SOC := sun8i-r40
 endef