voyager717/openwrt

commit 99be2bb9a2f9a8becb5a63771b691377cb9b0ac7

images: fix boot failures on NAND with small sub pages
SquashFS has a minimum block size of at least 1k, so we need to make
sure the last data block is also at least that big.

This is not an issue on NOR or SD CARD devices, since their rootfs
partitions go all the way to the end of the usable space.

But on NAND with ubiblock, the rootfs partition will be the exact space,
rounded up to LEB size. Unfortunately, some NAND chips with small sub
pages have a LEB size of x.5 kiB. This can cause the the last data block
to be less than 1k, which will cause the last block to be inaccessible,
causing boot failures as seen on MR24:

[    1.532960] block ubiblock0_3: created from ubi0:3(rootfs)
[    1.538457] ubiblock: device ubiblock0_3 (rootfs) set to be root filesystem
[    1.552847] SQUASHFS error: squashfs_read_data failed to read block 0x621472
[    1.559896] squashfs: SQUASHFS error: unable to read id index table
[    1.566474] VFS: Cannot open root device "(null)" or unknown-block(254,0): error -5

Since on most NOR devices, the start of the squashfs partition is not
aligned. Since the start of the rootfs_data partition there is dependend
on the SquashFS size, we cannot just always pad it, as the padding could
creep into the rootfs_data partition, breaking jffs2.

So fix this by ensuring a squashfs rootfs is always a multiple of 1k
only for UBI and NAND sysupgrade images.

Fixes #2460 without affecting NOR devices.

Tested-by: Russell Senior <russell@personaltelco.net>
Signed-off-by: Jonas Gorski <jonas.gorski@gmail.com>

scripts/functions.sh
@@ -1,0 +1,26 @@
+#!/bin/sh
+
+
+get_magic_word() {
+	dd if=$1 bs=4 count=1 2>/dev/null | od -A n -N 4 -t x1 | tr -d ' '
+}
+
+get_fs_type() {
+	local magic_word="$(get_magic_word "$1")"
+
+	case "$magic_word" in
+	"3118"*)
+		echo "ubifs"
+		;;
+	"68737173")
+		echo "squashfs"
+		;;
+	*)
+		echo "unknown"
+		;;
+	esac
+}
+
+round_up() {
+	echo "$(((($1 + ($2 - 1))/ $2) * $2))"
+}

scripts/sysupgrade-tar.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+. $TOPDIR/scripts/functions.sh
+
 board=""
 kernel=""
 rootfs=""
@@ -53,7 +55,16 @@ fi
 
 mkdir -p "${tmpdir}/sysupgrade-${board}"
 echo "BOARD=${board}" > "${tmpdir}/sysupgrade-${board}/CONTROL"
-[ -z "${rootfs}" ] || cp "${rootfs}" "${tmpdir}/sysupgrade-${board}/root"
+if [ -n "${rootfs}" ]; then
+	case "$( get_fs_type ${rootfs} )" in
+	"squashfs")
+		dd if="${rootfs}" of="${tmpdir}/sysupgrade-${board}/root" bs=1024 conv=sync
+		;;
+	*)
+		cp "${rootfs}" "${tmpdir}/sysupgrade-${board}/root"
+		;;
+	esac
+fi
 [ -z "${kernel}" ] || cp "${kernel}" "${tmpdir}/sysupgrade-${board}/kernel"
 
 mtime=""

scripts/ubinize-image.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+. $TOPDIR/scripts/functions.sh
+
 part=""
 ubootenv=""
 ubinize_param=""
@@ -7,16 +9,6 @@ kernel=""
 rootfs=""
 outfile=""
 err=""
-
-get_magic_word() {
-	dd if=$1 bs=2 count=1 2>/dev/null | od -A n -N 2 -t x1 | tr -d ' '
-}
-
-is_ubifs() {
-	if [ "$( get_magic_word $1 )" = "3118" ]; then
-		echo "1"
-	fi
-}
 
 ubivol() {
 	volid=$1
@@ -31,7 +23,7 @@ ubivol() {
 	echo "vol_name=$name"
 	if [ "$image" ]; then
 		echo "image=$image"
-		[ -n "$size" ] && echo "vol_size=${size}MiB"
+		[ -n "$size" ] && echo "vol_size=${size}"
 	else
 		echo "vol_size=1MiB"
 	fi
@@ -42,7 +34,10 @@ ubivol() {
 
 ubilayout() {
 	local vol_id=0
-	local root_is_ubifs="$( is_ubifs "$2" )"
+	local rootsize=
+	local autoresize=
+	local rootfs_type="$( get_fs_type "$2" )"
+
 	if [ "$1" = "ubootenv" ]; then
 		ubivol $vol_id ubootenv
 		vol_id=$(( $vol_id + 1 ))
@@ -62,16 +57,28 @@ ubilayout() {
 
 		size="$part"
 
-		ubivol $vol_id "$name" "$image" "" "$size"
+		ubivol $vol_id "$name" "$image" "" "${size}MiB"
 		vol_id=$(( $vol_id + 1 ))
 	done
 	if [ "$3" ]; then
 		ubivol $vol_id kernel "$3"
 		vol_id=$(( $vol_id + 1 ))
 	fi
-	ubivol $vol_id rootfs "$2" $root_is_ubifs
+
+	case "$rootfs_type" in
+	"ubifs")
+		autoresize=1
+		;;
+	"squashfs")
+		# squashfs uses 1k block size, ensure we do not
+		# violate that
+		rootsize="$( round_up "$( stat -c%s "$2" )" 1024 )"
+		;;
+	esac
+	ubivol $vol_id rootfs "$2" "$autoresize" "$rootsize"
+
 	vol_id=$(( $vol_id + 1 ))
-	[ "$root_is_ubifs" ] || ubivol $vol_id rootfs_data "" 1
+	[ "$rootfs_type" = "ubifs" ] || ubivol $vol_id rootfs_data "" 1
 }
 
 while [ "$1" ]; do

commit 34d37b6a9e44a1635bf295a82c5c3e3ce2aa3be9

images: Fix sysupgrade.tar for devices with NOR flash
The NOR flash rootfs images stored in a sysupgrade.tar must end with the
JFFS2 marker. Otherwise, devices like OpenMesh A42/A62 are not able to
calculate the md5sum of the fixed squashfs part and store it inside the
u-boot-env.

But the commit 99be2bb9a2f9 ("images: fix boot failures on NAND with small
sub pages") adds up to 1020 0x00 bytes after the 0xdead0de EOF marker. The
calculated md5sum will be wrong due do this change and u-boot will fail to
boot the newly flashed device with a message like:

  Validating MD5Sum of 'vmlinux'...
  Passed!
  Validating MD5Sum of 'rootfs'...
  Failed!
      583a1b7b54b8601efa64ade42742459b != 8850ee812dfd7638e94083329d5d2781

  Data validation failed!

and boot the old image again.

Since the original change should not change the behavior of NOR images,
just check for the deadc0de marker at the end of the squashfs-jffs2 image
do avoid the problematic behavior for these images.

Fixes: 99be2bb9a2f9 ("images: fix boot failures on NAND with small sub pages")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

scripts/functions.sh
@@ -5,6 +5,19 @@ get_magic_word() {
 	dd if=$1 bs=4 count=1 2>/dev/null | od -A n -N 4 -t x1 | tr -d ' '
 }
 
+get_post_padding_word() {
+	local rootfs_length="$(stat -c%s "$1")"
+	[ "$rootfs_length" -ge 4 ] || return
+	rootfs_length=$((rootfs_length-4))
+
+	# the JFFS2 end marker must be on a 4K boundary (often 64K or 256K)
+	local unaligned_bytes=$((rootfs_length%4096))
+	[ "$unaligned_bytes" = 0 ] || return
+
+	# skip rootfs data except the potential EOF marker
+	dd if="$1" bs=1 skip="$rootfs_length" 2>/dev/null | od -A n -N 4 -t x1 | tr -d ' '
+}
+
 get_fs_type() {
 	local magic_word="$(get_magic_word "$1")"
 
@@ -13,7 +26,16 @@ get_fs_type() {
 		echo "ubifs"
 		;;
 	"68737173")
-		echo "squashfs"
+		local post_padding_word="$(get_post_padding_word "$1")"
+
+		case "$post_padding_word" in
+		"deadc0de")
+			echo "squashfs-jffs2"
+			;;
+		*)
+			echo "squashfs"
+			;;
+		esac
 		;;
 	*)
 		echo "unknown"