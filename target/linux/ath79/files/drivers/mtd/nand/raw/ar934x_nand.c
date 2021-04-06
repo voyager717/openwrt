voyager717/openwrt

commit 97db173b19ad681ca786a2b67c0a8ad75846830b

ath79: ar934x-nand: add kernel 5.10 compatibility
Adapt the driver to make it work with the NAND subsystem changes between
kernel 5.4 and 5.10.

Tested-on: Aerohive HiveAP121

Signed-off-by: David Bauer <mail@david-bauer.net>

target/linux/ath79/files/drivers/mtd/nand/raw/ar934x_nand.c
@@ -14,6 +14,9 @@
 #include <linux/module.h>
 #include <linux/dma-mapping.h>
 #include <linux/mtd/mtd.h>
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,9,0)
+#include <linux/mtd/nand.h>
+#endif
 #include <linux/mtd/rawnand.h>
 #include <linux/mtd/partitions.h>
 #include <linux/platform_device.h>
@@ -719,7 +722,11 @@ static void ar934x_nfc_cmdfunc(struct nand_chip *nand, unsigned int command,
 		break;
 
 	case NAND_CMD_PAGEPROG:
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,9,0)
+		if (nand->ecc.engine_type == NAND_ECC_ENGINE_TYPE_ON_HOST) {
+#else
 		if (nand->ecc.mode == NAND_ECC_HW) {
+#endif
 			/* the data is already written */
 			break;
 		}
@@ -1325,15 +1332,29 @@ static int ar934x_nfc_attach_chip(struct nand_chip *nand)
 	if (ret)
 		return ret;
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,9,0)
+	if (nand->ecc.engine_type == NAND_ECC_ENGINE_TYPE_ON_HOST) {
+#else
 	if (nand->ecc.mode == NAND_ECC_HW) {
+#endif
 		ret = ar934x_nfc_setup_hwecc(nfc);
 		if (ret)
 			return ret;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,9,0)
+	} else if (nand->ecc.engine_type == NAND_ECC_ENGINE_TYPE_SOFT) {
+		dev_err(dev, "unknown ECC mode %d\n", nand->ecc.engine_type);
+#else
 	} else if (nand->ecc.mode != NAND_ECC_SOFT) {
 		dev_err(dev, "unknown ECC mode %d\n", nand->ecc.mode);
+#endif
 		return -EINVAL;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,9,0)
+	} else if ((nand->ecc.algo != NAND_ECC_ALGO_BCH) &&
+		   (nand->ecc.algo != NAND_ECC_ALGO_HAMMING)) {
+#else
 	} else if ((nand->ecc.algo != NAND_ECC_BCH) &&
 		   (nand->ecc.algo != NAND_ECC_HAMMING)) {
+#endif
 		dev_err(dev, "unknown software ECC algo %d\n", nand->ecc.algo);
 		return -EINVAL;
 	}
@@ -1422,7 +1443,11 @@ static int ar934x_nfc_probe(struct platform_device *pdev)
 	nand->legacy.read_byte = ar934x_nfc_read_byte;
 	nand->legacy.write_buf = ar934x_nfc_write_buf;
 	nand->legacy.read_buf = ar934x_nfc_read_buf;
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,9,0)
+	nand->ecc.engine_type = NAND_ECC_ENGINE_TYPE_ON_HOST;	/* default */
+#else
 	nand->ecc.mode = NAND_ECC_HW;	/* default */
+#endif
 	nand->priv = nfc;
 	platform_set_drvdata(pdev, nfc);
 
@@ -1462,7 +1487,12 @@ static int ar934x_nfc_remove(struct platform_device *pdev)
 
 	nfc = platform_get_drvdata(pdev);
 	if (nfc) {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,8,0)
+		mtd_device_unregister(nand_to_mtd(&nfc->nand_chip));
+		nand_cleanup(&nfc->nand_chip);
+#else
 		nand_release(&nfc->nand_chip);
+#endif
 		ar934x_nfc_free_buf(nfc);
 	}
 

commit 7d5ef875c596bb351849891c575325fb6cd553c0

ath79: ar934x-nand: fix broken nand on kernel 5.10
Booting current 5.10.x testing kernel shows this:

[    0.335781] nand: device found, Manufacturer ID: 0xec, Chip ID: 0xf1
[    0.342280] nand: Samsung NAND 128MiB 3,3V 8-bit
[    0.346982] nand: 128 MiB, SLC, erase size: 128 KiB, page size: 2048,
OOB size: 64
[    0.354703] ar934x-nand 1b800200.nand: unknown ECC mode 2
[    0.360193] ar934x-nand 1b800200.nand: nand_scan failed, err:-22
[    0.366341] ar934x-nand: probe of 1b800200.nand failed with error -22

...

[    1.089264] /dev/root: Can't open blockdev
[    1.093482] VFS: Cannot open root device "(null)" or
unknown-block(0,0): error -6
[    1.101077] Please append a correct "root=" boot option; here are the
available partitions:
[    1.109555] 1f00              64 mtdblock0
[    1.109559]  (driver?)
[    1.116199] 1f01              48 mtdblock1
[    1.116202]  (driver?)
[    1.122841] 1f02               4 mtdblock2
[    1.122844]  (driver?)
[    1.129493] 1f03               4 mtdblock3
[    1.129497]  (driver?)
[    1.136138] 1f04               4 mtdblock4
[    1.136142]  (driver?)
[    1.142787] Kernel panic - not syncing: VFS: Unable to mount root fs
on unknown-block(0,0)
[    1.151168] Rebooting in 1 seconds..

The issue is caused by a wrong check for ECC engine type
on newer kernels.

Fix this.

Bootlog after:

[    0.335689] nand: device found, Manufacturer ID: 0xec, Chip ID: 0xf1
[    0.342192] nand: Samsung NAND 128MiB 3,3V 8-bit
[    0.346884] nand: 128 MiB, SLC, erase size: 128 KiB, page size: 2048,
OOB size: 64
[    0.354611] Scanning device for bad blocks
[    0.364924] random: fast init done
[    0.496641] 3 fixed-partitions partitions found on MTD device
ar934x-nand
[    0.503545] Creating 3 MTD partitions on "ar934x-nand":
[    0.508865] 0x000000000000-0x000000040000 : "booter"
[    0.515133] 0x000000040000-0x000000400000 : "kernel"
[    0.523265] 0x000000400000-0x000008000000 : "ubi"

Fixes: 97db173b19 ("ath79: ar934x-nand: add kernel 5.10 compatibility")

target/linux/ath79/files/drivers/mtd/nand/raw/ar934x_nand.c
@@ -1341,7 +1341,7 @@ static int ar934x_nfc_attach_chip(struct nand_chip *nand)
 		if (ret)
 			return ret;
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5,9,0)
-	} else if (nand->ecc.engine_type == NAND_ECC_ENGINE_TYPE_SOFT) {
+	} else if (nand->ecc.engine_type != NAND_ECC_ENGINE_TYPE_SOFT) {
 		dev_err(dev, "unknown ECC mode %d\n", nand->ecc.engine_type);
 #else
 	} else if (nand->ecc.mode != NAND_ECC_SOFT) {