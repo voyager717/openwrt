voyager717/openwrt

commit 020b1e3411839c1b4aed8f73e0c29b478abacf8f

ath79: move TPLINK_HWID/_HWREV to parent for tplink-safeloader
Several devices with tplink-safeloader contain default values (0x0)
for TPLINK_HWID and TPLINK_HWREV in their device definitions.
Move those to common tplink-safeloader definition so they do not
have to be repeated each time.

While at it, set default value for tplink-v1 and tplink-v2 as well.

Signed-off-by: Adrian Schmutzler <freifunk@adrianschmutzler.de>

target/linux/ath79/image/generic-tp-link.mk
@@ -320,8 +320,6 @@ define Device/tplink_re350k-v1
   DEVICE_VARIANT := v1
   DEVICE_PACKAGES := kmod-ath10k-ct ath10k-firmware-qca988x-ct
   TPLINK_BOARD_ID := RE350K-V1
-  TPLINK_HWID := 0x0
-  TPLINK_HWREV := 0
 endef
 TARGET_DEVICES += tplink_re350k-v1
 
@@ -330,8 +328,6 @@ define Device/tplink_rex5x-v1
   SOC := qca9558
   IMAGE_SIZE := 6016k
   DEVICE_PACKAGES := kmod-ath10k-ct-smallbuffers ath10k-firmware-qca988x-ct
-  TPLINK_HWID := 0x0
-  TPLINK_HWREV := 0
 endef
 
 define Device/tplink_re355-v1
@@ -359,8 +355,6 @@ define Device/tplink_re450-v2
   DEVICE_MODEL := RE450
   DEVICE_VARIANT := v2
   DEVICE_PACKAGES := kmod-ath10k-ct-smallbuffers ath10k-firmware-qca988x-ct
-  TPLINK_HWID := 0x0
-  TPLINK_HWREV := 0
   TPLINK_BOARD_ID := RE450-V2
   LOADER_TYPE := elf
 endef
@@ -585,8 +579,6 @@ define Device/tplink_tl-wr902ac-v1
 	-swconfig -uboot-envtools
   TPLINK_BOARD_ID := TL-WR902AC-V1
   IMAGE_SIZE := 7360k
-  TPLINK_HWID := 0x0
-  TPLINK_HWREV := 0
   SUPPORTED_DEVICES += tl-wr902ac-v1
 endef
 TARGET_DEVICES += tplink_tl-wr902ac-v1

target/linux/ath79/image/common-tp-link.mk
@@ -11,6 +11,7 @@ endef
 
 define Device/tplink-v1
   DEVICE_VENDOR := TP-Link
+  TPLINK_HWID := 0x0
   TPLINK_HWREV := 0x1
   TPLINK_HEADER_VERSION := 1
   LOADER_TYPE := gz
@@ -23,6 +24,7 @@ endef
 
 define Device/tplink-v2
   DEVICE_VENDOR := TP-Link
+  TPLINK_HWID := 0x0
   TPLINK_HWREV := 0x1
   TPLINK_HWREVADD := 0x0
   TPLINK_HVERSION := 3
@@ -74,9 +76,10 @@ endef
 
 define Device/tplink-safeloader
   $(Device/tplink-v1)
+  TPLINK_HWREV := 0x0
   KERNEL := kernel-bin | append-dtb | lzma | tplink-v1-header -O
   IMAGE/sysupgrade.bin := append-rootfs | tplink-safeloader sysupgrade | \
-    append-metadata | check-size $$$$(IMAGE_SIZE)
+	append-metadata | check-size $$$$(IMAGE_SIZE)
   IMAGE/factory.bin := append-rootfs | tplink-safeloader factory
 endef
 

target/linux/ath79/image/tiny-tp-link.mk
@@ -95,7 +95,6 @@ define Device/tplink_tl-wa850re-v2
   DEVICE_VARIANT := v2
   TPLINK_BOARD_ID := TLWA850REV2
   TPLINK_HWID := 0x08500002
-  TPLINK_HWREV := 0x0
   DEVICE_PACKAGES := rssileds
   SUPPORTED_DEVICES += tl-wa850re-v2
 endef

commit 62b916590eb42bdd642f626885f6b28a43a850ff

ath79: fix TPLINK_HWREV field for TL-WR1043ND v4
Required to allow sysupgrades from OpenWrt 19.07.

Closes #7071

Fixes: 020b1e341183 ("ath79: move TPLINK_HWID/_HWREV to parent for tplink-safeloader")
Tested-by: J. Burfeind <git@aiyionpri.me>
Signed-off-by: Matthias Schiffer <mschiffer@universe-factory.net>

target/linux/ath79/image/generic-tp-link.mk
@@ -711,6 +711,7 @@ define Device/tplink_tl-wr1043nd-v4
   DEVICE_VARIANT := v4
   DEVICE_PACKAGES := kmod-usb2 kmod-usb-ledtrig-usbport
   TPLINK_HWID := 0x10430004
+  TPLINK_HWREV := 0x1
   TPLINK_BOARD_ID := TLWR1043NDV4
   SUPPORTED_DEVICES += tl-wr1043nd-v4
 endef