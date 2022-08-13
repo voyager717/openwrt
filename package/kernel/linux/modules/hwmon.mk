voyager717/openwrt

commit 1e7c5e9e8969e51786c34f6ee64aa17a2657dd5c

kernel: add kmod-hwmon-tps23861 support
Package the tps23861.ko module, which supports Texas Instruments
TPS23861 PoE+ controller.

Signed-off-by: Alexandru Gagniuc <mr.nuke.me@gmail.com>

package/kernel/linux/modules/hwmon.mk
@@ -519,6 +519,20 @@ endef
 $(eval $(call KernelPackage,hwmon-tmp421))
 
 
+define KernelPackage/hwmon-tps23861
+  TITLE:=Texas Instruments TPS23861 PoE PSE
+  KCONFIG:=CONFIG_SENSORS_TPS23861
+  FILES:=$(LINUX_DIR)/drivers/hwmon/tps23861.ko
+  AUTOLOAD:=$(call AutoProbe,tps23861)
+  $(call AddDepends/hwmon,+kmod-i2c-core)
+endef
+
+define KernelPackage/hwmon-tps23861/description
+  Kernel module for the Texas Instruments TPS23861 802.3at PoE PSE chips.
+endef
+
+$(eval $(call KernelPackage,hwmon-tps23861))
+
 define KernelPackage/hwmon-vid
   TITLE:=VID/VRM/VRD voltage conversion module.
   KCONFIG:=CONFIG_HWMON_VID

commit 21da02fb244e966cbaf5a0335fe5f7a57a2741f7

kernel: kmod-hwmon-tps23861: Add missing dependency to kmod-regmap-i2c
This fixes the following build error:
Package kmod-hwmon-tps23861 is missing dependencies for the following libraries:
regmap-i2c.ko

Fixes: 1e7c5e9e8969 ("kernel: add kmod-hwmon-tps23861 support")
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

package/kernel/linux/modules/hwmon.mk
@@ -524,7 +524,7 @@ define KernelPackage/hwmon-tps23861
   KCONFIG:=CONFIG_SENSORS_TPS23861
   FILES:=$(LINUX_DIR)/drivers/hwmon/tps23861.ko
   AUTOLOAD:=$(call AutoProbe,tps23861)
-  $(call AddDepends/hwmon,+kmod-i2c-core)
+  $(call AddDepends/hwmon,+kmod-i2c-core +kmod-regmap-i2c)
 endef
 
 define KernelPackage/hwmon-tps23861/description