voyager717/openwrt

commit ede5b8960a8f04f1742078cdcb988e6541717e64

ltq-vdsl-fw: update w921v firmware download URL
Update Telekom Speedport W921V firmware download URL.
Contained TAPI and VDSL firmware blobs are identical.

Signed-off-by: Daniel Golle <daniel@makrotopia.org>

package/kernel/lantiq/ltq-vdsl-fw/src/vdsl_fw_install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 FW="/tmp/firmware-speedport-w921v-1.46.000.bin"
-URL="https://www.telekom.de/hilfe/downloads/firmware-speedport-w921v-1.46.000.bin"
+URL="https://www.telekom.de/hilfe/downloads/firmware-speedport-w-921v-1.48.000.bin"
 FW_TAPI="vr9_tapi_fw.bin"
 FW_DSL="vr9_dsl_fw_annex_b.bin"
-MD5_FW="188734c0773b225f8c130984b279621a"
+MD5_FW="afa25146119daced87f67c9b66ef94b3"
 MD5_TAPI="57f2d07f59e11250ce1219bad99c1eda"
 MD5_DSL="655442e31deaa42c9c68944869361ec0"
 

package/kernel/lantiq/ltq-vdsl-fw/src/w921v_fw_cutter.c
@@ -26,7 +26,7 @@
 
 #include "LzmaWrapper.h"
 
-#define FW_NAME		"/tmp/firmware-speedport-w921v-1.46.000.bin"
+#define FW_NAME		"/tmp/firmware-speedport-w-921v-1.48.000.bin"
 
 #define MAGIC		0x50
 #define MAGIC_SZ	0x3FFC00
@@ -78,7 +78,7 @@ int main(int argc, char **argv)
 
 	if (stat(FW_NAME, &s) != 0) {
 		printf("Failed to find %s\n", FW_NAME);
-		printf("Ask Google or try https://www.telekom.de/hilfe/downloads/firmware-speedport-w921v-1.45.000.bin\n");
+		printf("Ask Google or try https://www.telekom.de/hilfe/downloads/firmware-speedport-w-921v-1.48.000.bin\n");
 		return -1;
 	}
 

package/kernel/lantiq/ltq-vdsl-fw/Makefile
@@ -7,7 +7,7 @@ include $(TOPDIR)/rules.mk
 
 PKG_NAME:=ltq-vdsl-fw
 PKG_VERSION:=6.8.6
-PKG_RELEASE:=3
+PKG_RELEASE:=4
 
 PKG_MAINTAINER:=Daniel Golle <daniel@makrotopia.org>
 

commit c6e1b4a361960c668c8ee050dffc7753f7d4598b

ltq-vdsl-fw: fix firmware installer
The downloaded filename was wrong in multiple places. Fix that.

Fixes: ede5b8960a ("ltq-vdsl-fw: update w921v firmware download URL")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

package/kernel/lantiq/ltq-vdsl-fw/src/vdsl_fw_install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-FW="/tmp/firmware-speedport-w921v-1.46.000.bin"
+FW="/tmp/firmware-speedport-w921v-1.48.000.bin"
 URL="https://www.telekom.de/hilfe/downloads/firmware-speedport-w-921v-1.48.000.bin"
 FW_TAPI="vr9_tapi_fw.bin"
 FW_DSL="vr9_dsl_fw_annex_b.bin"

package/kernel/lantiq/ltq-vdsl-fw/src/w921v_fw_cutter.c
@@ -26,7 +26,7 @@
 
 #include "LzmaWrapper.h"
 
-#define FW_NAME		"/tmp/firmware-speedport-w-921v-1.48.000.bin"
+#define FW_NAME		"/tmp/firmware-speedport-w921v-1.48.000.bin"
 
 #define MAGIC		0x50
 #define MAGIC_SZ	0x3FFC00