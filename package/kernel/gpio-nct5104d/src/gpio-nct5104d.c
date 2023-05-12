voyager717/openwrt

commit 2939ad8a8818d6a442471763df64e087761adeb7

kernel: gpio-nct5104d remove boardname check
'In different versions of coreboot are different names of apu boardname.
No need to check boardname to load module.'

Signed-off-by: Lukáš Mrtvý <lukas.mrtvy@gmail.com>

package/kernel/gpio-nct5104d/src/gpio-nct5104d.c
@@ -434,11 +434,6 @@ static int __init nct5104d_gpio_init(void)
 	const char *board_vendor = dmi_get_system_info(DMI_BOARD_VENDOR);
 	const char *board_name = dmi_get_system_info(DMI_BOARD_NAME);
 
- 	/* Make sure we only run on PC Engine APU boards */
-	if (!board_name || !board_vendor || strcasecmp(board_vendor, "PC Engines") || strncasecmp(board_name, "apu", 3)) {
-		return -ENODEV;
-	}
-
 	if (nct5104d_find(0x2e, &sio) &&
 	    nct5104d_find(0x4e, &sio))
 		return -ENODEV;

commit 3d6eab7e6115fd6077284c9bd43fa24cb25cb14f

kernel: gpio-nct5104d: drop unused variables in nct5104d_gpio_init
Drop unused variables in nct5104d_gpio_init. These variables should have
been dropped in 2939ad8a8818 ("kernel: gpio-nct5104d remove boardname check").
Thix fix compilation warning:
/__w/openwrt/openwrt/openwrt/build_dir/target-x86_64-openwrt-linux-musl_musl/linux-x86_64/gpio-nct5104d/gpio-nct5104d.c: In function 'nct5104d_gpio_init':
/__w/openwrt/openwrt/openwrt/build_dir/target-x86_64-openwrt-linux-musl_musl/linux-x86_64/gpio-nct5104d/gpio-nct5104d.c:413:21: error: unused variable 'board_name' [-Werror=unused-variable]
  413 |         const char *board_name = dmi_get_system_info(DMI_BOARD_NAME);
      |                     ^~~~~~~~~~
/__w/openwrt/openwrt/openwrt/build_dir/target-x86_64-openwrt-linux-musl_musl/linux-x86_64/gpio-nct5104d/gpio-nct5104d.c:412:21: error: unused variable 'board_vendor' [-Werror=unused-variable]
  412 |         const char *board_vendor = dmi_get_system_info(DMI_BOARD_VENDOR);
      |                     ^~~~~~~~~~~~
cc1: all warnings being treated as errors

Fixes: 2939ad8a8818 ("kernel: gpio-nct5104d remove boardname check")
Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

package/kernel/gpio-nct5104d/src/gpio-nct5104d.c
@@ -409,8 +409,6 @@ static int __init nct5104d_gpio_init(void)
 {
 	int err;
 	struct nct5104d_sio sio;
-	const char *board_vendor = dmi_get_system_info(DMI_BOARD_VENDOR);
-	const char *board_name = dmi_get_system_info(DMI_BOARD_NAME);
 
 	if (nct5104d_find(0x2e, &sio) &&
 	    nct5104d_find(0x4e, &sio))