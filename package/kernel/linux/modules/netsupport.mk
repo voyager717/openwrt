voyager717/openwrt

commit 6890a9f5442ed2a66650e93bc9c5ce429e0beb55

kernel: add mdio-bus-mux support
The MDIO bus multiplexing framework is used by some drivers
such as dwmac-sun8i.

As this is a per-driver requirement, set it to be hidden in the menu.

Signed-off-by: Mathew McBride <matt@traverse.com.au>

package/kernel/linux/modules/netsupport.mk
@@ -1363,6 +1363,21 @@ endef
 
 $(eval $(call KernelPackage,mdio))
 
+define KernelPackage/mdio-bus-mux
+  SUBMENU:=$(NETWORK_SUPPORT_MENU)
+  TITLE:=MDIO bus multiplexers
+  KCONFIG:=CONFIG_MDIO_BUS_MUX
+  HIDDEN:=1
+  FILES:=$(LINUX_DIR)/drivers/net/mdio/mdio-mux.ko
+  AUTOLOAD:=$(call AutoLoad,32,mdio-mux)
+endef
+
+define KernelPackage/mdio/description
+ Kernel framework for MDIO bus multiplexers.
+endef
+
+$(eval $(call KernelPackage,mdio-bus-mux))
+
 define KernelPackage/macsec
   SUBMENU:=$(NETWORK_SUPPORT_MENU)
   TITLE:=IEEE 802.1AE MAC-level encryption (MAC)

commit b084b9e03f363bde5b60e2a14a7f0be71d04f1e0

kernel: modules: fix mdio-bus-mux description
Simple error during copy/paste

Fixes: 6890a9f5442e ("kernel: add mdio-bus-mux support")
Signed-off-by: Luiz Angelo Daros de Luca <luizluca@gmail.com>

package/kernel/linux/modules/netsupport.mk
@@ -1372,7 +1372,7 @@ define KernelPackage/mdio-bus-mux
   AUTOLOAD:=$(call AutoLoad,32,mdio-mux)
 endef
 
-define KernelPackage/mdio/description
+define KernelPackage/mdio-bus-mux/description
  Kernel framework for MDIO bus multiplexers.
 endef
 